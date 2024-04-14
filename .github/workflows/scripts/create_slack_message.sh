2f9329b7cd611bfaa6b0c869270379587fa1e8da
selva2tech <166980238+selva2tech@users.noreply.github.com>
2024-04-14 21:27:46 +0530

    Update create_slack_message.sh

--- .github/workflows/scripts/create_slack_message.sh
+++ .github/workflows/scripts/create_slack_message.sh
@@ -3,10 +3,11 @@
 set -x
 
 MSG_CONTENTS=slack_msg.txt
+APPEND_MSG=./github/message_note.txt
 
 echo -e "TEST MESSAGE:" > $SLACK_MSG_CONTENTS
 
-cat $MESSAGE >> $MSG_CONTENTS
+cat $APPEND_MSG >> $MSG_CONTENTS
 
 echo 'SLACK_MESSAGE<<EOF' >> $GITHUB_ENV
 echo "$(cat $MSG_CONTENTS)" >> $GITHUB_ENV
